c336e31a052e74f036899a1b2c7273834e2bffbe
Matheus Duarte de Freitas <freickup@gmail.com>
2021-05-30 06:22:27 -0300

    finalizaçao dos exercicios do dia 5.1

---
+++ exercices/bloco5/5.2/script.js
@@ -0,0 +1,8 @@
+let body = document.querySelector('body');
+
+let bodyFirstTitle = document.createElement('h1');
+
+bodyFirstTitle.innerText = 'Exercício 5.2 - JavaScript DOM';
+
+body.appendChild(bodyFirstTitle);
+
